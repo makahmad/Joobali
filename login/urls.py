762ff40a616389e2f3585df2d999565bb134f57c
Rongjian Lan <rongjian.lan@gmail.com>
2016-09-16 15:09:44 -0700

    add session support and complete login/logout/signup flow

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from login import views
 
 urlpatterns = [
-	url(r'^$', views.form),
+	url(r'^signup', views.signup),
+	url(r'^logout', views.logout),
+	url(r'^$', views.login),
 	url(r'^home', views.home),
 ]
--- login/views.py
+++ login/views.py
@@ -6,6 +6,7 @@ from wtforms_appengine.ndb import model_form
 from django.http import HttpResponse
 
 from login import models
+from gaesessions import get_current_session
 
 import dwollav2
 
@@ -42,7 +43,16 @@ ProviderForm = model_form(models.Provider, field_args={
 	},
 })
 
-def form(request):
+LoginForm = model_form(models.Provider, field_args={
+	'email': {
+		'filters': [stripFilter],
+	},
+	'password': {
+		'filters': [stripFilter],
+	}
+})
+
+def signup(request):
 	form = ProviderForm()
 	if request.method == 'POST':
 		provider = models.Provider()
@@ -51,7 +61,6 @@ def form(request):
 		print form.email.errors
 		print form.email.filters
 		form.validate()
-		print 
 		if form.validate():
 			form.populate_obj(provider)
 			query = models.Provider.query().filter(models.Provider.email == provider.email)
@@ -60,21 +69,54 @@ def form(request):
 				form.email.errors.append('error: user exists')
 			else:
 				provider.put()
+				request.session['email'] = provider.email
 				print "INFO: successfully stored Provider:" + str(provider)
 
-				client = dwollav2.Client(id = 'g36djuD0XBwoDteIjEz9fcGKsKJbWN72IW8wmXBZA5glcSUhg9', secret = '3clqlV4LrOf7udsCjuYs9ONnN1Eq78a0OcNvpUWcCBK5PTNkQ9', environment = 'sandbox')
-				account_token = client.Token(access_token = 'UZjwsTujbiEVxi0egVgWHACt1vT5tQckyE1uj1gaqNxwL0TwOB', refresh_token = 'o9tuD34y19J7yw86lDratuOCdD4Ngmq5xqOLJqTiBAIK4LEqke')
-				request_body = {
-				  'firstName': provider.firstName,
-				  'lastName': provider.lastName,
-				  'email': provider.email,
-				  'ipAddress': '99.99.99.99'
-				}
-				customer = account_token.post('customers', request_body)
-				return HttpResponseRedirect('/login')
+
+				return HttpResponseRedirect('/home')
 
 	return render_to_response(
 		'login/userform.html',
 		{'form': form},
 		template.RequestContext(request)
 	)
+
+def login(request):
+	form = LoginForm()
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
+		form.validate()
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+		print 'FORM:::::::'
+		print email
+		print password
+		if email and password:
+			print 'haha'
+			query = models.Provider.query().filter(models.Provider.email == email)
+			result = query.fetch(1)
+			print result
+			if not result:
+				form.email.errors.append('error: user does not exist')
+			elif result[0].password == password:
+				# authentication succeeded.
+				print 'login successful'
+				request.session['email'] = email
+				print request.session
+				return HttpResponseRedirect('/home')
+			else:
+				form.email.errors.append('error: password wrong')
+
+	return render_to_response(
+		'login/login.html',
+		{'form': form},
+		template.RequestContext(request)
+	)
+
+def logout(request):
+	loggedIn = False
+	if request.session.get('email'):
+		loggedIn = True
+	if loggedIn:
+		request.session.terminate()
+	return HttpResponseRedirect('/home')
