47924ace21e1daae0fbf84c25ae80941a653af1f
ZILONG HUANG <huangxx_2155@qq.com>
2016-09-28 23:26:38 -0700

    store enrollment in backend.

--- enrollment/views.py
+++ enrollment/views.py
@@ -7,13 +7,14 @@ from . import models
 from manageprogram.views import JEncoder
 import json
 import logging
+import re
 
 EnrollmentForm = model_form(models.Enrollment)
 logger = logging.getLogger(__name__)
 
 
 def render_enrollment_home(request):
-    """handles user's request to the enrollment page"""
+    """Handles user's request to get enrollment page"""
     logger.info("rendering the enrollment home")
 
     return render(
@@ -25,20 +26,45 @@ def render_enrollment_home(request):
 
 
 def add_enrollment(request):
-    logger.info("add enrollment")
+    """Handles HttpRequest about adding a new enrollment"""
+    logger.info(request)
     if request.method != 'POST':
+        logger.info("get non-post http request")
         return
-    enrollment_form = EnrollmentForm(request.POST)
+    logger.info("request.body %s", request.body)
+    enrollment_form = EnrollmentForm(data=convert_post_enrollment_data(json.loads(request.body)))
+    logger.info("enrollment_form %s" % enrollment_form.data)
+    status = ""
     if enrollment_form.validate():
         enrollment = models.Enrollment()
         enrollment_form.populate_obj(enrollment)
         enrollment.status = "initiated"
         enrollment.put()
-    return HttpResponseRedirect('/enrollment')
+        status = "success"
+    else:
+        logger.info("enrollment_form errors: [%s]" % enrollment_form.errors)
+        logger.info("enrollment_form not valid")
+        status = "failure"
+    return HttpResponse(json.dumps({'status': status}), content_type="application/json")
 
 
 def list_enrollment(request):
-    """handles user's request to list all enrollment"""
+    """Handles user's request to list all enrollment"""
     # TODO(zilong): add query filter on the current child care provider
     enrollments = models.Enrollment.query()
     return HttpResponse(json.dumps([JEncoder().encode(enrollment) for enrollment in enrollments]))
+
+
+def convert_post_enrollment_data(enrollment_data):
+    """Converts frontend form key into backend form key"""
+    new_enrollment_data = dict()
+    logger.info(enrollment_data)
+    for key in enrollment_data:
+        new_enrollment_data[convert_camel_case_to_snake_case(key)] = enrollment_data[key]
+    return new_enrollment_data
+
+
+def convert_camel_case_to_snake_case(name):
+    """Converts Camel Case Name to Snake Case Name"""
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
--- static/enrollment/program_app.js
+++ static/enrollment/program_app.js
@@ -1,34 +1,9 @@
+'use strict';
 
-EnrollmentController = function($scope, $http) {
-	console.log('EnrollmentController running');
-	this.scope_ = $scope;
-    $scope.num = 0;
-	console.log($scope);
-    this.addEnrollment();
+angular.module('enrollmentApp', [])
+    .config(['$httpProvider',
+        function($httpProvider) {
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        }]);
 
-    $http({
-	  method: 'GET',
-	  url: '/enrollment/list'
-	}).then(angular.bind(this, function successCallback(response) {
-	    // this callback will be called asynchronously
-	    // when the response is available
-	    console.log(response);
-	    this.scope_.enrollments = [];
-	    angular.forEach(response.data, angular.bind(this, function(enrollment) {
-	    	this.scope_.enrollments.push(JSON.parse(enrollment));
-	    }));
-	    console.log(this.scope_.enrollments);
-
-	  }), function errorCallback(response) {
-	    // called asynchronously if an error occurs
-	    // or server returns response with an error status.
-	    console.log(response);
-	  });
-};
-
-EnrollmentController.prototype.addEnrollment = function() {
-    console.log("EnrollmentController.protoType.addEnrollment()");
-};
-
-app = angular.module('enrollmentApp', []);
-app.controller('EnrollmentCtrl', EnrollmentController);
