336cdca6811f048ae26c370a4ba74f674bb04865
Zilong <zilong@joobali.com>
2017-04-23 16:22:01 -0700

    Add ages in child-card, and set max date for date of birth when adding child

--- child/static/child/child-card.component.js
+++ child/static/child/child-card.component.js
@@ -8,8 +8,20 @@ ChildCardController = function ChildCardController($uibModal, $scope, $http, $ro
 
 ChildCardController.prototype.$onInit = function() {
     this.getEnrollmentData();
-    console.log('this.child is ' + this.child);
-    console.log('this.programs is ' + this.programs);
+    console.log(moment(this.child.date_of_birth, "MM/DD/YYYY").toNow(true));
+    var currentDate = moment(new Date());
+    var dateDiff = moment.duration(currentDate.diff(moment(this.child.date_of_birth, "MM/DD/YYYY")));
+    diff_str = "";
+    if (dateDiff.years() > 0) {
+        diff_str += dateDiff.years() + " years, "
+    }
+    if (dateDiff.months() > 0) {
+        diff_str += dateDiff.months() + " months, "
+    }
+    if (dateDiff.days() > 0) {
+        diff_str += dateDiff.days() + " days"
+    }
+    this.child.age = diff_str;
 };
 
 ChildCardController.prototype.openEnrollmentModal = function() {
@@ -44,7 +56,6 @@ ChildCardController.prototype.getEnrollmentData = function() {
     this.http_.post('/enrollment/listByChildId', { 'child_id' : this.child.id })
     .then(angular.bind(this, function successCallback(response) {
         this.enrollments = [];
-        console.log('enrollment/listByChild: ' + response.data)
         angular.forEach(response.data, angular.bind(this, function(enrollment) {
             this.enrollments.push(JSON.parse(enrollment));
         }));
--- child/static/child/child-list.component.js
+++ child/static/child/child-list.component.js
@@ -24,12 +24,10 @@ ChildListController.prototype.refreshList = function() {
     }).then(angular.bind(this, function successCallback(response) {
         // this callback will be called asynchronously
         // when the response is available
-        console.log(response);
         this.children = [];
         angular.forEach(response.data, angular.bind(this, function(child) {
             this.children.push(JSON.parse(child));
         }));
-        console.log(this.children);
     }), angular.bind(this, function errorCallback(response) {
         // TODO(zilong): deal with error here
         console.log(response);
