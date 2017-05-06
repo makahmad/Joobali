ec5450e7139db4cba6be02cd67763d1224856080
makjoobali <mak@joobali.com>
2017-05-05 22:11:57 -0700

    accidental check in of launch script

--- launch_local_server.sh
+++ launch_local_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/Users/seyed.ahmad/Downloads/google-cloud-sdk/platform/google_appengine/dev_appserver.py app_dev.yaml\
+dev_appserver.py app_dev.yaml\
   --datastore_path='~/joobali_local_data_store' \
   --show_mail_body=true \
