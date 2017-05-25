35933805cb389f86d30ca92a6782197072bf45a4
Zilong <zilong@joobali.com>
2017-05-24 19:09:08 -0700

    Add a provider_verfication_status page

--- verification/views.py
+++ verification/views.py
@@ -1,17 +1,31 @@
-from django.http import HttpResponse
+from django import template
+from django.shortcuts import render_to_response
 
 from verification_util import get_provider_email_verification_token
+
 
 def verify_provider_email(request):
     if request.method != 'GET':
         return
     token_id = request.GET['t']
     token = get_provider_email_verification_token(token_id)
+    context = {
+        'verification_status': "unsuccessful",
+        'email': "N/A",
+        'school_name': "N/A"
+    }
     if token is not None:
         provider = token.provider_key.get()
         provider.status.status = 'active'
         provider.put()
-        token.key.delete()
-        return HttpResponse('Verification Successful')
-    return HttpResponse('Verification Failed')
+        # token.key.delete()
+        context["status"] = 'successful'
+        context["email"] = provider.email
+        context["schoolName"] = provider.schoolName
+        context["loginUrl"] = request.get_host() + "/login"
 
+    return render_to_response(
+        'verification/provider_verification_status.html',
+        context,
+        template.RequestContext(request)
+    )
