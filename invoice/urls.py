6bbcddb726c10f8cbe7637aad45eaf29d706a971
Rongjian Lan <rongjian.lan@gmail.com>
2017-06-01 23:32:10 -0700

    Add payment cancel function; Fixes emailing bugs

--- invoice/urls.py
+++ invoice/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	url(r'^listinvoices', views.listInvoices, name='listInvoices'),
 	url(r'^setupautopay', views.setupAutopay, name='setupAutopay'),
 	url(r'^cancelautopay', views.cancelAutopay, name='cancelAutopay'),
+	url(r'^cancelpayment', views.cancelPayment, name='cancelPayment'),
 	url(r'^markpaid', views.markPaid, name='markPaid'),
 	url(r'^viewinvoice', views.viewInvoice, name='viewInvoice'),
 	url(r'^addinvoice', views.add_invoice, name='addInvoice'),
