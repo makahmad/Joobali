f97a57d06f19dc55098fdb711e8f6d5ee0cdd96f
Rongjian Lan <rongjian.lan@gmail.com>
2017-12-04 21:41:39 -0800

    Fix Daylight saving time display issue

--- common/datetime_util.py
+++ common/datetime_util.py
@@ -1,5 +1,6 @@
 """Package for common timezone related method"""
 import pytz
+from datetime import timedelta
 
 def local_to_utc(date_time):
     if date_time is None:
@@ -13,4 +14,8 @@ def utc_to_local(date_time):
         return None
     # TODO: get timezone from user.
     localized_time = pytz.timezone('US/Pacific').localize(date_time)
-    return date_time + localized_time.utcoffset()
+    local_time = date_time + localized_time.utcoffset()
+    if local_time.hour == 23 and local_time.minute == 0 and local_time.second == 0:
+        return local_time + timedelta(hours=1)
+    else:
+        return local_time
