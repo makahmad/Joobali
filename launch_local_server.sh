72b94477c8f7cb9a5fd7082820bee90471ff51db
cassandraabad <cassandra@joobali.com>
2018-08-28 16:38:33 -0700

    test

--- launch_local_server.sh
+++ launch_local_server.sh
@@ -3,6 +3,6 @@
 # Add --enable_sendmail to enable actual mail sending. Please make sure you update app_dev.yaml with the expected email
 # address to send and receive email
 
-/Users/brentpantell/Downloads/google-cloud-sdk/platform/google_appengine/dev_appserver.py app_dev.yaml \
+/Users/cassandraabad/Downloads/google-cloud-sdk/platform/google_appengine/dev_appserver.py app_dev.yaml \
   --datastore_path='~/joobali_local_data_store' \
   --show_mail_body=true
